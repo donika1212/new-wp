e1ecc005228820768858f324d446ab421a4d526c
donika1212 <donika.pllana@digitalschool.tech>
2025-05-05 16:58:18 +0200

    Update content-latest-news.php

--- parts/content-latest-news.php
+++ parts/content-latest-news.php
@@ -1,6 +1,6 @@
 <article class="latest-news">
-                                        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'large' ); ?></a>
-                                        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'large' ); ?></a>
+    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                                         <div class="meta-info">
                                         <p>
                                             by <span><?php the_author_posts_link(); ?></span> 
@@ -10,4 +10,4 @@
                                         <p><span><?php echo get_the_date(); ?></p>
                                         </div>
                                         <?php the_excerpt(); ?>
-                                    </article>
+</article>
